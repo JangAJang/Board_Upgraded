b3a202a6e4df7aa8e0002deb94aadfbf93272b93
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-07-27 08:29:11 +0900

    initial commit

--- board_update/board_upgrade/src/main/java/com/example/board_upgrade/BoardUpgradeApplication.java
+++ board_update/board_upgrade/src/main/java/com/example/board_upgrade/BoardUpgradeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BoardUpgradeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BoardUpgradeApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BoardUpgradeApplication.class, args);
+	}
 
 }
---
+++ board_update/board_upgrade/src/main/java/com/example/board_upgrade/Configuration/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.example.board_upgrade.Configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket restAPI(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Board RestAPI ")
+                .version("1.0.0")
+                .description("게시판 업그레이드 버전의 API를 위한 Swagger의 info입니다. ")
+                .build();
+    }
+}
