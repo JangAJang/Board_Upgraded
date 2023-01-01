b816d2f3155c4973492f9b233b096dea18e7ea59
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-01 16:07:53 +0900

    feat: NeedToLoginException Authentication이 널값으로 로그인이 필요함에 대한 커스텀 예외처리 추가

--- src/main/java/com/board/Board_Upgraded/exception/ExceptionAdvice.java
+++ src/main/java/com/board/Board_Upgraded/exception/ExceptionAdvice.java
@@ -1,5 +1,7 @@
 package com.board.Board_Upgraded.exception;
 
+import com.board.Board_Upgraded.exception.authentication.NeedToLoginException;
+import com.board.Board_Upgraded.exception.authentication.NotRightAuthenticationException;
 import com.board.Board_Upgraded.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,15 +11,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(NeedToLoginException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Response needToLoginException(){
         return Response.failure(403, "인증이 되어있지 않습니다. 로그인하세요.");
     }
 
-    @ExceptionHandler(RuntimeException.class)
+    @ExceptionHandler(NotRightAuthenticationException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Response notRightAuthentication(){
+    public Response notRightAuthenticationException(){
         return Response.failure(403, "인증 정보가 존재하지 않습니다. 다시 로그인해주세요");
     }
 
