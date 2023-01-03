b2d63869fd2dcac64c33751cdfd4a94b0e7ce474
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-03 23:37:29 +0900

    feat: TokenProvider Access토큰을 가지고 인증 정보를 반환하는 기능 구현

--- src/main/java/com/board/Board_Upgraded/config/jwt/TokenProvider.java
+++ src/main/java/com/board/Board_Upgraded/config/jwt/TokenProvider.java
@@ -6,13 +6,21 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
+
+import static com.board.Board_Upgraded.dto.token.TokenGeneratingComponent.AUTHORITIES_KEY;
 
 @Slf4j
 @Component
@@ -50,5 +58,22 @@ public class TokenProvider {
             log.info("JWT 토큰이 잘못되었습니다.");
         }
         return false;
+    }
+
+    public Authentication getAuthentication(String accessToken) {
+        // 토큰 복호화
+        Claims claims = parseClaims(accessToken);
+        if (claims.get(AUTHORITIES_KEY.getComponent()) == null) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
+        // 클레임에서 권한 정보 가져오기
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(claims.get(AUTHORITIES_KEY.getComponent()).toString().split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+        // UserDetails 객체를 만들어서 Authentication 리턴
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 }
