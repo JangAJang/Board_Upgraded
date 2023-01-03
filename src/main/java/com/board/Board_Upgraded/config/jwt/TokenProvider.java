e4748930521bec7b487b8fc77fc307d807a6202f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-03 21:42:52 +0900

    feat: TokenProvider 토큰 생산자의 생성자에서 정해진 암호키로 암호화된 토큰을 생성하는 기능 구현

---
+++ src/main/java/com/board/Board_Upgraded/config/jwt/TokenProvider.java
@@ -0,0 +1,26 @@
+package com.board.Board_Upgraded.config.jwt;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Slf4j
+@Component
+public class TokenProvider {
+
+    private static final String AUTHORITIES_KEY = "auth";
+    private static final String BEARER_TYPE = "bearer";
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24;            // 1시간 * 24 = 24시간(-Dev)
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
+    private final Key key;
+
+    public TokenProvider(@Value("${jwt.secret}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
