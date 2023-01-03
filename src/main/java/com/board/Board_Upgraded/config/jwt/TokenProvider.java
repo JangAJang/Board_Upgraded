d74708810b67ab6de10783a40d5698c06fb78473
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-03 23:31:22 +0900

    refactor: TokenDto 인증정보와 키를 TokenProvider에서 생성받아 TokenDto에서 이를 입력받아 도메인에서 처리하는 방식으로 토큰 생성 로직 수정

--- src/main/java/com/board/Board_Upgraded/config/jwt/TokenProvider.java
+++ src/main/java/com/board/Board_Upgraded/config/jwt/TokenProvider.java
@@ -18,11 +18,6 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 public class TokenProvider {
-
-    private static final String AUTHORITIES_KEY = "auth";
-    private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24;            // 1시간 * 24 = 24시간(-Dev)
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
     private final Key key;
 
     public TokenProvider(@Value("${jwt.secret}") String secretKey) {
@@ -31,37 +26,6 @@ public class TokenProvider {
     }
 
     public TokenDto generateTokenDto(Authentication authentication) {
-        Date accessTokenExpiresIn = new Date((new Date()).getTime() + ACCESS_TOKEN_EXPIRE_TIME);
-        return TokenDto.builder()
-                .grantType(BEARER_TYPE)
-                .accessToken(generateAccessToken(authentication, accessTokenExpiresIn))
-                .accessTokenExpiresIn(accessTokenExpiresIn.getTime())
-                .refreshToken(generateRefreshToken())
-                .build();
-    }
-
-    // Refresh Token 생성
-    private String generateRefreshToken(){
-        return Jwts.builder()
-                .setExpiration(new Date((new Date()).getTime() + REFRESH_TOKEN_EXPIRE_TIME))
-                .signWith(key, SignatureAlgorithm.HS512)
-                .compact();
-    }
-
-    // Access Token 생성
-    private String generateAccessToken(Authentication authentication, Date accessTokenExpiresIn){
-        return Jwts.builder()
-                .setSubject(authentication.getName())       // payload "sub": "name"
-                .claim(AUTHORITIES_KEY, getAuthorities(authentication))        // payload "auth": "ROLE_USER"
-                .setExpiration(accessTokenExpiresIn)        // payload "exp": 1516239022 (예시)
-                .signWith(key, SignatureAlgorithm.HS512)    // header "alg": "HS512"
-                .compact();
-    }
-
-    // 권한들 가져오기
-    private String getAuthorities(Authentication authentication){
-        return authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority)
-                .collect(Collectors.joining(","));
+        return new TokenDto(authentication, key);
     }
 }
--- src/main/java/com/board/Board_Upgraded/dto/token/TokenDto.java
+++ src/main/java/com/board/Board_Upgraded/dto/token/TokenDto.java
@@ -1,9 +1,16 @@
 package com.board.Board_Upgraded.dto.token;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import static com.board.Board_Upgraded.dto.token.TokenGeneratingComponent.*;
 
 @Getter
 @NoArgsConstructor
@@ -14,5 +21,37 @@ public class TokenDto {
     private String grantType;
     private String accessToken;
     private String refreshToken;
-    private Long accessTokenExpiresIn;
+    private Date accessTokenExpiresIn;
+
+    public TokenDto(Authentication authentication, Key secretKey){
+        this.accessTokenExpiresIn = new Date((new Date()).getTime() + Long.parseLong(ACCESS_TOKEN_EXPIRE_TIME.getComponent()));
+        this.grantType = BEARER_TYPE.getComponent();
+        this.accessToken = generateAccessToken(authentication, secretKey);
+        this.refreshToken = generateRefreshToken(secretKey);
+    }
+
+    // 권한들 가져오기
+    private String getAuthorities(Authentication authentication){
+        return authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+    }
+
+    // Access Token 생성
+    private String generateAccessToken(Authentication authentication, Key key){
+        return Jwts.builder()
+                .setSubject(authentication.getName())       // payload "sub": "name"
+                .claim(AUTHORITIES_KEY.getComponent(), getAuthorities(authentication))        // payload "auth": "ROLE_USER"
+                .setExpiration(accessTokenExpiresIn)        // payload "exp": 1516239022 (예시)
+                .signWith(key, SignatureAlgorithm.HS512)    // header "alg": "HS512"
+                .compact();
+    }
+
+    // Refresh Token 생성
+    private String generateRefreshToken(Key key){
+        return Jwts.builder()
+                .setExpiration(new Date((new Date()).getTime() + Long.parseLong(REFRESH_TOKEN_EXPIRE_TIME.getComponent())))
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+    }
 }
