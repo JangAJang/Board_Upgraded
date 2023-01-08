48fcafd30c467c4b5cc3aff8b6177ab716ae6f33
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-08 13:41:17 +0900

    feat: SecurityConfig 비밀번호 암호화를 위한 encode기능 구현

--- src/main/java/com/board/Board_Upgraded/config/SecurityConfig.java
+++ src/main/java/com/board/Board_Upgraded/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -16,6 +17,11 @@ public class SecurityConfig{
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/api/test/getTest");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encode(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
