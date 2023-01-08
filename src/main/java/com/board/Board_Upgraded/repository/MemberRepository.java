8d486cebba77682ae2c6887b81b6e3a84375e83e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-08 13:46:36 +0900

    feat: MemberRepository 데이터베이스에서 멤버 테이블을 사용하기 위한 멤버 레포지토리 구현

---
+++ src/main/java/com/board/Board_Upgraded/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.board.Board_Upgraded.repository;
+
+
+import com.board.Board_Upgraded.entity.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByUsername(String username);
+
+    Optional<Member> findByNickname(String nickname);
+
+    Optional<Member> findByEmail(String email);
+}
