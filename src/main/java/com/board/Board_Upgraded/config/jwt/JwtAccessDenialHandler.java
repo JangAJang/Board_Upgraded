1db34eecfa73d1317f21a5b0b2669175fd832010
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-09 13:55:53 +0900

    refactor: JWT AccessDenialHandler과 AuthenticationEntryPoint에서 예외에 대해 로그 추가하는 기능 추가

--- src/main/java/com/board/Board_Upgraded/config/jwt/JwtAccessDenialHandler.java
+++ src/main/java/com/board/Board_Upgraded/config/jwt/JwtAccessDenialHandler.java
@@ -1,5 +1,6 @@
 package com.board.Board_Upgraded.config.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -7,13 +8,17 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
+@Slf4j
 public class JwtAccessDenialHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException{
+        LocalDateTime now = LocalDateTime.now();
+        log.error("time : " + now + "\nrequest" + request + "\nException" + accessDeniedException.getMessage());
         response.sendError(403);
     }
 }
--- src/main/java/com/board/Board_Upgraded/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/board/Board_Upgraded/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.board.Board_Upgraded.config.jwt;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -7,14 +8,18 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @Component
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
+        LocalDateTime now = LocalDateTime.now();
+        log.error("time : " + now + "\nrequest" + request + "\nException" + authException.getMessage());
         response.sendError(401);
     }
 }
