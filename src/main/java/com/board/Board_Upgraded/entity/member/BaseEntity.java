7adccda8851fabf0a987a646cf4a441b46d1c98d
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-09 14:49:13 +0900

    feat: BaseEntity 아이디와 생성시간, 수정시간을 가지는 회원 엔티티 추가

---
+++ src/main/java/com/board/Board_Upgraded/entity/member/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.board.Board_Upgraded.entity.member;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "CREATED_TIME", nullable = false, updatable = false)
+    private LocalDateTime createdTime;
+
+
+    @Column(name = "LAST_MODIFIED_TIME", nullable = false)
+    private LocalDateTime lastModifiedTime;
+}
