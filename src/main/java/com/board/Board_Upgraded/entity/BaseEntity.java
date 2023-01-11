d54395b4a38101bfba52da5aba0aecfef88e2db7
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-11 16:16:39 +0900

    feat: BaseEntity 최종 수정일이 일정 기간 이상이면 참을 반환하는 기능 구현

--- src/main/java/com/board/Board_Upgraded/entity/BaseEntity.java
+++ src/main/java/com/board/Board_Upgraded/entity/BaseEntity.java
@@ -1,11 +1,14 @@
 package com.board.Board_Upgraded.entity;
 
+import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@Data
 public class BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,9 +17,13 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(name = "CREATED_TIME", nullable = false, updatable = false)
-    private LocalDateTime createdTime;
+    private LocalDateTime createdDate;
 
 
     @Column(name = "LAST_MODIFIED_TIME", nullable = false)
-    private LocalDateTime lastModifiedTime;
+    private LocalDateTime lastModifiedDate;
+
+    public boolean isLastModifiedDateAfter(Long days){
+        return LocalDateTime.now().minusDays(days).isAfter(this.lastModifiedDate);
+    }
 }
