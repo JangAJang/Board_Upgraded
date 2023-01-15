dca34b1ccf39489298160a41d5dc399fbb79f6ac
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-15 18:07:04 +0900

    style: ExceptionAdvice에서 예외 종류가 많아 각 로직과 관련된 엔티티로 예외 패키지 분류

--- src/main/java/com/board/Board_Upgraded/exception/member/MemberExceptionAdvice.java
+++ src/main/java/com/board/Board_Upgraded/exception/member/MemberExceptionAdvice.java
@@ -1,10 +1,5 @@
-package com.board.Board_Upgraded.exception;
+package com.board.Board_Upgraded.exception.member;
 
-import com.board.Board_Upgraded.exception.authentication.NeedToLoginException;
-import com.board.Board_Upgraded.exception.authentication.NotAuthenticationInfoException;
-import com.board.Board_Upgraded.exception.authentication.NotRightAuthenticationException;
-import com.board.Board_Upgraded.exception.authentication.WrongTokenException;
-import com.board.Board_Upgraded.exception.member.*;
 import com.board.Board_Upgraded.response.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,31 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ExceptionAdvice {
-
-    @ExceptionHandler(NeedToLoginException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Response needToLoginException(){
-        return Response.failure(403, "인증이 되어있지 않습니다. 로그인하세요.");
-    }
-
-    @ExceptionHandler(NotRightAuthenticationException.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Response notRightAuthenticationException(){
-        return Response.failure(403, "인증 정보가 존재하지 않습니다. 다시 로그인해주세요");
-    }
-
-    @ExceptionHandler(NotAuthenticationInfoException.class)
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public Response notAuthenticationInfoException(){
-        return Response.failure(403, "권한 정보가 없는 토큰입니다.");
-    }
-
-    @ExceptionHandler(WrongTokenException.class)
-    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public Response wrongTokenException(){
-        return Response.failure(401, "JWT 토큰이 잘못되었습니다.");
-    }
+public class MemberExceptionAdvice {
 
     @ExceptionHandler(UsernameAlreadyInUseException.class)
     @ResponseStatus(HttpStatus.FOUND)
