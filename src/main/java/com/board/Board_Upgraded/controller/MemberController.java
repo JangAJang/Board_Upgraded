163a9496d3b5ba7fe8b02fc2889164099e222f97
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-22 15:00:53 +0900

    feat: MemberController 닉네임 변경 로직을 수행하는 기능 구현

--- src/main/java/com/board/Board_Upgraded/controller/MemberController.java
+++ src/main/java/com/board/Board_Upgraded/controller/MemberController.java
@@ -1,15 +1,22 @@
 package com.board.Board_Upgraded.controller;
 
+import com.board.Board_Upgraded.dto.member.ChangeNicknameRequestDto;
+import com.board.Board_Upgraded.dto.member.MemberInfoDto;
 import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
 import com.board.Board_Upgraded.dto.member.SignInRequestDto;
 import com.board.Board_Upgraded.dto.token.TokenDto;
 import com.board.Board_Upgraded.dto.token.TokenResponseDto;
+import com.board.Board_Upgraded.entity.member.Member;
+import com.board.Board_Upgraded.exception.member.MemberNotFoundException;
+import com.board.Board_Upgraded.repository.MemberRepository;
 import com.board.Board_Upgraded.response.Response;
 import com.board.Board_Upgraded.service.MemberService;
 import com.board.Board_Upgraded.service.RefreshTokenService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +31,7 @@ public class MemberController {
     private final MemberService memberService;
 
     private final RefreshTokenService refreshTokenService;
+    private final MemberRepository memberRepository;
 
     @ApiOperation(value = "회원가입", notes = "회원가입 페이지 입니다.")
     @ResponseStatus(HttpStatus.OK)
@@ -38,5 +46,19 @@ public class MemberController {
     @PostMapping("/signIn")
     public TokenResponseDto signIn(SignInRequestDto signInRequestDto){
         return refreshTokenService.createTokenDtoByAuthentication(memberService.getAuthenticationToSignIn(signInRequestDto));
+    }
+
+    @ApiOperation(value = "닉네임 변경", notes = "회원의 닉네임을 변경하는 기능 구현")
+    @ResponseStatus(HttpStatus.OK)
+    @PostMapping("/changeNickname")
+    public MemberInfoDto changeNickname(ChangeNicknameRequestDto changeNicknameRequestDto){
+        Member member = findMemberByUsername();
+        memberService.changeMemberNickname(changeNicknameRequestDto, member);
+        return new MemberInfoDto(member);
+    }
+
+    private Member findMemberByUsername(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return memberRepository.findByUsername(authentication.getName()).orElseThrow(MemberNotFoundException::new);
     }
 }
