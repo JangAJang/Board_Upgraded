bdab0e4bca2f3b61fc1e9b47913c67fd29996d69
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-04 21:12:14 +0900

    refactor: SearchMemberDto 검색대상에 이메일과 아이디 추가

--- src/main/java/com/board/Board_Upgraded/dto/member/SearchMemberDto.java
+++ src/main/java/com/board/Board_Upgraded/dto/member/SearchMemberDto.java
@@ -11,4 +11,10 @@ public class SearchMemberDto {
 
     @NotNull(message = "검색할 대상의 닉네임을 입력해주세요.")
     private String nickname;
+
+    @NotNull(message = "검색할 대상의 이메일을 입력하세요.")
+    private String email;
+
+    @NotNull(message = "검색할 대상의 아이디를 입력하세요.")
+    private String username;
 }
