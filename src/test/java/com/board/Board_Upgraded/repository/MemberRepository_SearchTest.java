0962a1fb2b883b1e0901051a1f040922fe78f500
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-05 16:33:01 +0900

    refactor: MemberRepositoryTest에서 MemberRepository_SearchTest 분리 및 검색 결과 List<SearchMemberDto>로 변경

---
+++ src/test/java/com/board/Board_Upgraded/repository/MemberRepository_SearchTest.java
@@ -0,0 +1,137 @@
+package com.board.Board_Upgraded.repository;
+
+import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
+import com.board.Board_Upgraded.dto.member.SearchMemberDto;
+import com.board.Board_Upgraded.entity.member.Member;
+import com.board.Board_Upgraded.repository.member.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+public class MemberRepository_SearchTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @BeforeEach
+    void createData() {
+        for(int index = 0; index < 10; index++){
+            RegisterRequestDto registerRequestDto = RegisterRequestDto.builder()
+                    .username("testUser" + index)
+                    .nickname("test" + index)
+                    .email("test" + index + "@test.com")
+                    .password("password" + index)
+                    .passwordCheck("password" + index)
+                    .build();
+            Member member = new Member(registerRequestDto);
+            memberRepository.save(member);
+            em.flush();
+            em.clear();
+        }
+    }
+
+    @Test
+    @DisplayName("아이디로만 검색했을 때 검색한 멤버의 닉네임이 같은 번호로 나온다. ")
+    public void searchByUsernameTest() throws Exception{
+        //given
+        SearchMemberDto searchMemberDto = new SearchMemberDto("testUser3", null, null);
+
+        //when
+        List<SearchMemberDto> search = memberRepository.search(searchMemberDto);
+        //then
+        assertThat(search.get(0).getNickname()).isEqualTo("test3");
+    }
+
+    @Test
+    @DisplayName("닉네임으로만 검색했을 때 검색한 멤버의 아이디가 같은 번호로 나온다. ")
+    public void searchByNickname() throws Exception{
+        //given
+
+        SearchMemberDto searchMemberDto = new SearchMemberDto(null, "test3", null);
+
+        //when
+        List<SearchMemberDto> search = memberRepository.search(searchMemberDto);
+        //then
+        assertThat(search.get(0).getUsername()).isEqualTo("testUser3");
+    }
+
+    @Test
+    @DisplayName("이메일로만 검색했을 때 검색한 멤버의 아이디가 같은 번호로 나온다. ")
+    public void searchByEmail() throws Exception{
+        //given
+
+        SearchMemberDto searchMemberDto = new SearchMemberDto(null, null, "test3@test.com");
+
+        //when
+        List<SearchMemberDto> search = memberRepository.search(searchMemberDto);
+        //then
+        assertThat(search.get(0).getUsername()).isEqualTo("testUser3");
+    }
+
+    @Test
+    @DisplayName("아이디와 닉네임으로 검색하면 같은 번호의 이메일이 나온다.")
+    public void searchByUsernameAndNickname() throws Exception{
+        //given
+
+        SearchMemberDto searchMemberDto = new SearchMemberDto("testUser3", "test3", null);
+
+        //when
+        List<SearchMemberDto> search = memberRepository.search(searchMemberDto);
+        //then
+        assertThat(search.get(0).getEmail()).isEqualTo("test3@test.com");
+    }
+
+    @Test
+    @DisplayName("아이디와 이메일로 검색하면 같은 번호의 닉네임이 나온다.")
+    public void searchByUsernameAndEmail() throws Exception{
+        //given
+
+        SearchMemberDto searchMemberDto = new SearchMemberDto("testUser3", null, "test3@test.com");
+
+        //when
+        List<SearchMemberDto> search = memberRepository.search(searchMemberDto);
+        //then
+        assertThat(search.get(0).getNickname()).isEqualTo("test3");
+    }
+
+    @Test
+    @DisplayName("닉네임과 이메일로 검색하면 같은 번호의 아이디가 나온다.")
+    public void searchByNicknameAndEmail() throws Exception{
+        //given
+
+        SearchMemberDto searchMemberDto = new SearchMemberDto(null, "test3", "test3@test.com");
+
+        //when
+        List<SearchMemberDto> search = memberRepository.search(searchMemberDto);
+        //then
+        assertThat(search.get(0).getUsername()).isEqualTo("testUser3");
+    }
+
+    @Test
+    @DisplayName("모든 요소로 검색하면 같은 번호의 비밀번호가 나온다. ")
+    public void searchByAll() throws Exception{
+        //given
+        SearchMemberDto searchMemberDto = new SearchMemberDto("testUser3", "test3", "test3@test.com");
+
+        //when
+        List<SearchMemberDto> search = memberRepository.search(searchMemberDto);
+
+        //then
+        assertThat(search.get(0).getUsername()).isEqualTo("testUser3");
+        assertThat(search.get(0).getNickname()).isEqualTo("test3");
+        assertThat(search.get(0).getEmail()).isEqualTo("test3@test.com");
+    }
+}
