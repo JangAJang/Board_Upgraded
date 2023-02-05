f2a8b24eb37f88f8a6d45eafc8a46512b902bb27
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-05 13:33:57 +0900

    feat: build.gradle querydsl 의존성 주입 및 기본 설정 추가

---
+++ build/generated/querydsl/com/board/Board_Upgraded/entity/member/QMember.java
@@ -0,0 +1,56 @@
+package com.board.Board_Upgraded.entity.member;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = -1103857893L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final com.board.Board_Upgraded.entity.base.QBaseEntity _super = new com.board.Board_Upgraded.entity.base.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final StringPath email = createString("email");
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> lastModifiedDate = _super.lastModifiedDate;
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final EnumPath<Role> role = createEnum("role", Role.class);
+
+    public final StringPath username = createString("username");
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
