683dca6f542e47b4d4189bfa432bcfd9d43bb898
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-05 16:59:08 +0900

    refactor: SecuriryConfig BCryptEncoder -> PasswordEncoder로 반환타입 변경

--- src/main/java/com/board/Board_Upgraded/config/SecurityConfig.java
+++ src/main/java/com/board/Board_Upgraded/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -20,7 +21,7 @@ public class SecurityConfig{
     }
 
     @Bean
-    public BCryptPasswordEncoder encode(){
+    public PasswordEncoder encode(){
         return new BCryptPasswordEncoder();
     }
 
