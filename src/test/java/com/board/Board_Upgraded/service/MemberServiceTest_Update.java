5163efc1f319e5a5fc2442cc147e5479b9d55912
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-06 21:19:36 +0900

    feat: MemberServiceTest_Update 닉네임 변경시에 중복으로 취소되는 경우와 성공하는 경우에 대한 테스트코드 구현'

---
+++ src/test/java/com/board/Board_Upgraded/service/MemberServiceTest_Update.java
@@ -0,0 +1,70 @@
+package com.board.Board_Upgraded.service;
+
+import com.board.Board_Upgraded.dto.member.ChangeNicknameRequestDto;
+import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
+import com.board.Board_Upgraded.entity.member.Member;
+import com.board.Board_Upgraded.exception.member.NicknameAlreadyInUseException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceTest_Update {
+
+    @Autowired
+    private MemberService memberService;
+
+    private Member makeMember1(){
+        RegisterRequestDto registerRequestDto = RegisterRequestDto.builder()
+                .username("test1")
+                .nickname("test1")
+                .email("test1@test.com")
+                .password("password1")
+                .passwordCheck("password1")
+                .build();
+        memberService.registerNewMember(registerRequestDto);
+        return new Member(registerRequestDto);
+    }
+
+    private Member makeMember2(){
+        RegisterRequestDto registerRequestDto = RegisterRequestDto.builder()
+                .username("test2")
+                .nickname("test2")
+                .email("test2@test.com")
+                .password("password2")
+                .passwordCheck("password2")
+                .build();
+        memberService.registerNewMember(registerRequestDto);
+        return new Member(registerRequestDto);
+    }
+
+    @Test
+    @DisplayName("")
+    public void 닉네임_변경_중복() throws Exception{
+        //given
+        makeMember1();
+        Member member2 = makeMember2();
+        //when
+        ChangeNicknameRequestDto changeNicknameRequestDto = new ChangeNicknameRequestDto("test1");
+        //then
+        Assertions.assertThatThrownBy(()-> memberService.changeMemberNickname(changeNicknameRequestDto, member2))
+                .isInstanceOf(NicknameAlreadyInUseException.class);
+    }
+
+    @Test
+    @DisplayName("")
+    public void 닉네임_변경_성공() throws Exception{
+        //given
+        makeMember1();
+        Member member2 = makeMember2();
+        //when
+        ChangeNicknameRequestDto changeNicknameRequestDto = new ChangeNicknameRequestDto("test3");
+        //then
+        memberService.changeMemberNickname(changeNicknameRequestDto, member2);
+    }
+}
