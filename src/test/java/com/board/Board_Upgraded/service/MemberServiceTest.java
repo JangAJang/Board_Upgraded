9ccee7866b84201363fc8f6555bbcbd74a8c960c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-06 19:22:55 +0900

    feat: MemberServiceTest 회원가입에 대한 결과값 예상하여 서비스 로직 테스트 메서드 생성'

--- src/test/java/com/board/Board_Upgraded/service/MemberServiceTest.java
+++ src/test/java/com/board/Board_Upgraded/service/MemberServiceTest.java
@@ -4,6 +4,8 @@ import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
 import com.board.Board_Upgraded.entity.member.Member;
 import com.board.Board_Upgraded.repository.member.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -26,5 +28,60 @@ public class MemberServiceTest {
             Member member = new Member(registerRequestDto);
             memberRepository.save(member);
         }
+    }
+
+    @Test
+    @DisplayName("")
+    public void 아이디_중복() throws Exception{
+        //given
+
+        //when
+
+        //then
+
+    }
+    
+    @Test
+    @DisplayName("")        
+    public void 닉네임_중복() throws Exception{
+        //given
+        
+        //when
+        
+        //then
+        
+    }
+    
+    @Test
+    @DisplayName("")        
+    public void 이메일_중복() throws Exception{
+        //given
+        
+        //when
+        
+        //then
+        
+    }
+    
+    @Test
+    @DisplayName("")        
+    public void 비밀번호_불일치() throws Exception{
+        //given
+        
+        //when
+        
+        //then
+        
+    }
+    
+    @Test
+    @DisplayName("")        
+    public void 회원가입_정상동작() throws Exception{
+        //given
+        
+        //when
+        
+        //then
+        
     }
 }
