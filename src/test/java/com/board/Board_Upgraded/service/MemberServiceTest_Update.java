a89cf60b082bf25d22205a6c241c50c0a4a750c1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-06 21:28:16 +0900

    feat: MemberServiceTest 비밀번호 변경의 기능에 대해 입력이 달라 예외처리, 이전과 같아 예외처리, 성공하는 것을 검증하는 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/service/MemberServiceTest_Update.java
+++ src/test/java/com/board/Board_Upgraded/service/MemberServiceTest_Update.java
@@ -2,10 +2,13 @@ package com.board.Board_Upgraded.service;
 
 import com.board.Board_Upgraded.dto.member.ChangeEmailRequestDto;
 import com.board.Board_Upgraded.dto.member.ChangeNicknameRequestDto;
+import com.board.Board_Upgraded.dto.member.ChangePasswordRequestDto;
 import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
 import com.board.Board_Upgraded.entity.member.Member;
 import com.board.Board_Upgraded.exception.member.EmailAlreadyInUseException;
 import com.board.Board_Upgraded.exception.member.NicknameAlreadyInUseException;
+import com.board.Board_Upgraded.exception.member.PasswordNotChangedException;
+import com.board.Board_Upgraded.exception.member.PasswordNotMatchingException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -94,5 +97,46 @@ public class MemberServiceTest_Update {
         ChangeEmailRequestDto changeEmailRequestDto = new ChangeEmailRequestDto("test3@test.com");
         //then
         memberService.changeMemberEmail(changeEmailRequestDto, member2);
+    }
+
+    @Test
+    @DisplayName("")
+    public void 비밀번호_변경_기존과_동일() throws Exception{
+        //given
+        makeMember1();
+        Member member2 = makeMember2();
+        //when
+        ChangePasswordRequestDto changePasswordRequestDto
+                = new ChangePasswordRequestDto("password2", "password2");
+        //then
+        assertThatThrownBy(()-> memberService.changeMemberPassword(changePasswordRequestDto, member2))
+                .isInstanceOf(PasswordNotChangedException.class);
+    }
+
+    @Test
+    @DisplayName("")
+    public void 비밀번호_변경_입력이_서로_불일치() throws Exception{
+        //given
+        makeMember1();
+        Member member2 = makeMember2();
+        //when
+        ChangePasswordRequestDto changePasswordRequestDto
+                = new ChangePasswordRequestDto("password2", "password3");
+        //then
+        assertThatThrownBy(()-> memberService.changeMemberPassword(changePasswordRequestDto, member2))
+                .isInstanceOf(PasswordNotMatchingException.class);
+    }
+
+    @Test
+    @DisplayName("")
+    public void 비밀번호_변경_성공() throws Exception{
+        //given
+        makeMember1();
+        Member member2 = makeMember2();
+        //when
+        ChangePasswordRequestDto changePasswordRequestDto
+                = new ChangePasswordRequestDto("password3", "password3");
+        //then
+        memberService.changeMemberPassword(changePasswordRequestDto, member2);
     }
 }
