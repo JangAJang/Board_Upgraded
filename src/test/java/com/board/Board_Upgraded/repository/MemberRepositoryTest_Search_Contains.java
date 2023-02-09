0e79c1f35fb6fedd896e604a33be91880d1beb47
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-09 14:27:29 +0900

    feat: MemberRepositoryTest_Search_Contains 검색 dto의 각 인스턴스가 null이 아닐 때에 대한 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/repository/MemberRepositoryTest_Search_Contains.java
+++ src/test/java/com/board/Board_Upgraded/repository/MemberRepositoryTest_Search_Contains.java
@@ -40,7 +40,7 @@ public class MemberRepositoryTest_Search_Contains {
     }
 
     @Test
-    @DisplayName("testUser1로 검색하면, 1, 10, 11, 12, 13, 14, 15, 16, 17, 18이 1페이지에 나온다. ")
+    @DisplayName("testUser1로 검색하면, 1, 10, 11, 12, 13, 14, 15, 16, 17, 18이 1페이지에 나온다. 19, 100은 2페이지에 나온다")
     public void searchWith_testUser1() throws Exception{
         //given
         SearchMemberDto searchMemberDto = new SearchMemberDto("testUser1", null, null);
@@ -56,5 +56,60 @@ public class MemberRepositoryTest_Search_Contains {
                         "testUser14", "testUser15", "testUser16", "testUser17", "testUser18");
         assertThat(result2.getContent().stream().map(SearchMemberDto::getUsername))
                 .containsExactly("testUser19", "testUser100");
+    }
+
+    @Test
+    @DisplayName("test1으로 검색하면 위와 같은 아이디가 나온다. ")
+    public void searchWith_test1() throws Exception{
+        //given
+        SearchMemberDto searchMemberDto = new SearchMemberDto(null, "test1", null);
+        PageRequest pageRequest1 = PageRequest.of(0, 10);
+        PageRequest pageRequest2 = PageRequest.of(1, 10);
+        //when
+        Page<SearchMemberDto> result1 = memberRepository.search(searchMemberDto, pageRequest1, CONTAINS);
+        Page<SearchMemberDto> result2 = memberRepository.search(searchMemberDto, pageRequest2, CONTAINS);
+        System.out.println("result1 : " + result1.getContent().toString());
+        //then
+        assertThat(result1.getContent().stream().map(SearchMemberDto::getUsername))
+                .containsExactly("testUser1", "testUser10", "testUser11", "testUser12", "testUser13",
+                        "testUser14", "testUser15", "testUser16", "testUser17", "testUser18");
+        assertThat(result2.getContent().stream().map(SearchMemberDto::getUsername))
+                .containsExactly("testUser19", "testUser100");
+    }
+    @Test
+    @DisplayName("test1을 이메일로 검색하면 위와 같은 아이디가 나온다. ")
+    public void searchWith_test1Email() throws Exception{
+        //given
+        SearchMemberDto searchMemberDto = new SearchMemberDto(null, null, "test1");
+        PageRequest pageRequest1 = PageRequest.of(0, 10);
+        PageRequest pageRequest2 = PageRequest.of(1, 10);
+        //when
+        Page<SearchMemberDto> result1 = memberRepository.search(searchMemberDto, pageRequest1, CONTAINS);
+        Page<SearchMemberDto> result2 = memberRepository.search(searchMemberDto, pageRequest2, CONTAINS);
+        System.out.println("result1 : " + result1.getContent().toString());
+        //then
+        assertThat(result1.getContent().stream().map(SearchMemberDto::getUsername))
+                .containsExactly("testUser1", "testUser10", "testUser11", "testUser12", "testUser13",
+                        "testUser14", "testUser15", "testUser16", "testUser17", "testUser18");
+        assertThat(result2.getContent().stream().map(SearchMemberDto::getUsername))
+                .containsExactly("testUser19", "testUser100");
+    }
+
+    @Test
+    @DisplayName("test를 이메일으로 검색하면 전체가 조회되고, 첫 페이지에는 1~10이 나온다. ")
+    public void searchWith_testEmail() throws Exception{
+        //given
+        SearchMemberDto searchMemberDto = new SearchMemberDto(null, null, "test");
+        PageRequest pageRequest1 = PageRequest.of(0, 10);
+        PageRequest pageRequest2 = PageRequest.of(1, 10);
+        //when
+        Page<SearchMemberDto> result1 = memberRepository.search(searchMemberDto, pageRequest1, CONTAINS);
+        Page<SearchMemberDto> result2 = memberRepository.search(searchMemberDto, pageRequest2, CONTAINS);
+        System.out.println("result1 : " + result1.getContent().toString());
+        //then
+        assertThat(result1.getTotalElements()).isEqualTo(100);
+        assertThat(result1.getContent().stream().map(SearchMemberDto::getNickname))
+                .containsExactly("test1", "test2", "test3", "test4", "test5",
+                        "test6", "test7", "test8", "test9", "test10");
     }
 }
