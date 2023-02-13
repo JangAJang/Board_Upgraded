aab940f9221d9c3701dd95fd4a63034d08ef26f9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-13 22:45:03 +0900

    feat: MemberControllerTest_Search 조회 컨트롤러 로직을 테스트하기 위한 클래스 생성

---
+++ src/test/java/com/board/Board_Upgraded/controller/MemberControllerTest_Search.java
@@ -0,0 +1,15 @@
+package com.board.Board_Upgraded.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MemberControllerTest_Search {
+
+    @BeforeEach
+    void makeNewData(){
+
+    }
+}
