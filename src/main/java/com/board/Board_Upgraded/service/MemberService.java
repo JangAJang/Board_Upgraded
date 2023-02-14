0855a503efc51a1da2b69d457730cb24484220c1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-14 17:33:06 +0900

    fix: 컴파일과정에서 생성자 파라미터 문제로, 서비스에서 가져오는 MemberInstanceValidator는 @PostConstruct로 후에 초기화하게 수정

--- src/main/java/com/board/Board_Upgraded/service/MemberService.java
+++ src/main/java/com/board/Board_Upgraded/service/MemberService.java
@@ -2,7 +2,6 @@ package com.board.Board_Upgraded.service;
 
 import com.board.Board_Upgraded.dto.member.*;
 import com.board.Board_Upgraded.entity.member.Member;
-import com.board.Board_Upgraded.exception.member.*;
 import com.board.Board_Upgraded.repository.member.MemberRepository;
 import com.board.Board_Upgraded.repository.member.SearchType;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +11,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@RequiredArgsConstructor
+import javax.annotation.PostConstruct;
+
 @Service
+@RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
-    private final MemberInstanceValidator memberInstanceValidator;
+    private MemberInstanceValidator memberInstanceValidator;
+
+    @PostConstruct
+    private void setValidator(){
+        this.memberInstanceValidator = new MemberInstanceValidator(memberRepository, passwordEncoder);
+    }
 
     @Transactional
     public void changeMemberEmail(ChangeEmailRequestDto changeEmailRequestDto, Member member){
