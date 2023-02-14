dc371fd4c764f891a430c2753483b5803e48165b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-14 14:03:32 +0900

    feat: MemberControllerTest_Search 검색기능 테스트를 위해 더미데이터를 만드는 기능 구현

--- src/test/java/com/board/Board_Upgraded/controller/MemberControllerTest_Search.java
+++ src/test/java/com/board/Board_Upgraded/controller/MemberControllerTest_Search.java
@@ -1,15 +1,36 @@
 package com.board.Board_Upgraded.controller;
 
+import com.board.Board_Upgraded.entity.member.Member;
+import com.board.Board_Upgraded.repository.member.MemberRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.stream.IntStream;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MemberControllerTest_Search {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
     @BeforeEach
     void makeNewData(){
-
+        memberRepository.deleteAll();
+        IntStream.range(1, 31)
+                .forEach( i -> memberRepository.save(
+                        Member.builder()
+                                .username("test"+i)
+                                .nickname("test"+i)
+                                .email("test"+i+"@test.com")
+                                .password("test")
+                                .password("test")
+                                .build()));
     }
 }
