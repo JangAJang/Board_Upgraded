bc22d6166fcb8bfab828d539e176f030d0ef144a
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-14 14:17:40 +0900

    feat: MemberControllerTest_Search 검색을 요청했을 때, 로그인이 되어 헤더에 토큰이 없을 경우, 401에러를 반환하는 것을 검증하는 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/controller/MemberControllerTest_Search.java
+++ src/test/java/com/board/Board_Upgraded/controller/MemberControllerTest_Search.java
@@ -1,8 +1,10 @@
 package com.board.Board_Upgraded.controller;
 
+import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
 import com.board.Board_Upgraded.dto.member.SearchMemberDto;
 import com.board.Board_Upgraded.entity.member.Member;
 import com.board.Board_Upgraded.repository.member.MemberRepository;
+import com.board.Board_Upgraded.service.MemberService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -17,6 +19,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.stream.IntStream;
 
+import static com.board.Board_Upgraded.repository.member.SearchType.EXACT;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MemberControllerTest_Search {
@@ -25,25 +29,28 @@ public class MemberControllerTest_Search {
     private MockMvc mvc;
 
     @Autowired
+    private MemberService memberService;
+
+    @Autowired
     private MemberRepository memberRepository;
 
     @BeforeEach
     void makeNewData(){
         memberRepository.deleteAll();
         IntStream.range(1, 31)
-                .forEach( i -> memberRepository.save(
-                        Member.builder()
+                .forEach( i -> memberService.registerNewMember(
+                        RegisterRequestDto.builder()
                                 .username("test"+i)
                                 .nickname("test"+i)
                                 .email("test"+i+"@test.com")
                                 .password("test")
-                                .password("test")
+                                .passwordCheck("test")
                                 .build()));
     }
 
     @Test
-    @DisplayName("")
-    public void searchExceptionTest() throws Exception{
+    @DisplayName("로그인이 되어있지 않은 상태에서 조회를 시행하면, 권한이 없어 401에러를 반환한다. ")
+    public void searchTest_Unauthorized() throws Exception{
         //given
         SearchMemberDto searchMemberDto = SearchMemberDto.builder()
                 .username("")
@@ -51,10 +58,10 @@ public class MemberControllerTest_Search {
                 .email("")
                 .build();
         //expected
-        mvc.perform(MockMvcRequestBuilders.get("/members")
+        mvc.perform(MockMvcRequestBuilders.get("/members/{type}?page=0", EXACT)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(makeJson(makeJson(searchMemberDto))))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 
     private String makeJson(Object object){
