54e3197a137082729497b7099eee264909e7682a
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-14 14:10:30 +0900

    feat: MemberControllerTest_Search 검색기능이 200으로 성공하는지 검증하는 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/controller/MemberControllerTest_Search.java
+++ src/test/java/com/board/Board_Upgraded/controller/MemberControllerTest_Search.java
@@ -1,12 +1,19 @@
 package com.board.Board_Upgraded.controller;
 
+import com.board.Board_Upgraded.dto.member.SearchMemberDto;
 import com.board.Board_Upgraded.entity.member.Member;
 import com.board.Board_Upgraded.repository.member.MemberRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.stream.IntStream;
 
@@ -32,5 +39,29 @@ public class MemberControllerTest_Search {
                                 .password("test")
                                 .password("test")
                                 .build()));
+    }
+
+    @Test
+    @DisplayName("")
+    public void searchExceptionTest() throws Exception{
+        //given
+        SearchMemberDto searchMemberDto = SearchMemberDto.builder()
+                .username("")
+                .nickname("")
+                .email("")
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.get("/members")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(makeJson(searchMemberDto))))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    private String makeJson(Object object){
+        try{
+            return new ObjectMapper().writeValueAsString(object);
+        }catch(Exception e){
+            return "";
+        }
     }
 }
