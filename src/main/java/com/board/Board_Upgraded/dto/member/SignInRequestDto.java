d7660cda3e4758b2b02b537b11a42a57f87e868e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-15 16:57:54 +0900

    refactor: SignInRequestDto validation으로 인한 메시지 통일 및 공백문자열일 때에도 예외처리되게 코드 추가

--- src/main/java/com/board/Board_Upgraded/dto/member/SignInRequestDto.java
+++ src/main/java/com/board/Board_Upgraded/dto/member/SignInRequestDto.java
@@ -3,6 +3,7 @@ package com.board.Board_Upgraded.dto.member;
 import lombok.*;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
@@ -11,10 +12,12 @@ import javax.validation.constraints.NotNull;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SignInRequestDto {
 
-    @NotNull(message = "아이디를 입력해주세요.")
+    @NotNull(message = "아이디를 입력해야 합니다.")
+    @NotBlank(message = "아이디를 입력해야 합니다.")
     private String username;
 
-    @NotNull(message = "비밀번호를 입력해주세요.")
+    @NotNull(message = "비밀번호를 입력해야 합니다.")
+    @NotBlank(message = "비밀번호를 입력해야 합니다.")
     private String password;
 
     public UsernamePasswordAuthenticationToken toAuthentication() {
