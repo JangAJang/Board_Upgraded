a822085907b5ba2ae09e51df568002e55cd98c9a
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-15 22:58:11 +0900

    feat: TokenPath 토큰을 헤더에 설정하거나 헤더에서 가져오기 위한 클래스 생성

---
+++ src/main/java/com/board/Board_Upgraded/config/jwt/TokenPath.java
@@ -0,0 +1,43 @@
+package com.board.Board_Upgraded.config.jwt;
+
+import com.board.Board_Upgraded.dto.token.ReissueRequestDto;
+import com.board.Board_Upgraded.dto.token.TokenResponseDto;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TokenPath {
+
+    public ReissueRequestDto getReissueResponseDtoFromHeader(HttpServletRequest request){
+        return ReissueRequestDto.builder()
+                .accessToken(resolveAccessToken(request))
+                .refreshToken(resolveRefreshToken(request))
+                .build();
+    }
+
+    public void putTokensOnHeader(HttpServletResponse response, TokenResponseDto tokenResponseDto){
+        setHeaderAccessToken(response, tokenResponseDto.getAccessToken());
+        setHeaderRefreshToken(response, tokenResponseDto.getRefreshToken());
+    }
+
+    private void setHeaderAccessToken(HttpServletResponse response, String accessToken) {
+        response.setHeader("Authorization", "Bearer "+ accessToken);
+    }
+
+    // 리프레시 토큰 헤더 설정
+    private void setHeaderRefreshToken(HttpServletResponse response, String refreshToken) {
+        response.setHeader("RefreshToken", "Bearer "+ refreshToken);
+    }
+
+    private String resolveAccessToken(HttpServletRequest request) {
+        if(request.getHeader("Authorization") != null )
+            return request.getHeader("Authorization").substring(7);
+        return null;
+    }
+
+    private String resolveRefreshToken(HttpServletRequest request) {
+        if(request.getHeader("RefreshToken") != null )
+            return request.getHeader("RefreshToken").substring(7);
+        return null;
+    }
+}
