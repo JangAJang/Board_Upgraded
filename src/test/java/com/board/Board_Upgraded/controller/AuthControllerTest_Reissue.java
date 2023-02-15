af7acb057034e78b1101098b271c80fc8230721c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-15 21:43:57 +0900

    feat: AuthControllerTest_Reissue 토큰 재발행을 실행하기 위한 기본적인 메서드 생성

--- src/test/java/com/board/Board_Upgraded/controller/AuthControllerTest_Reissue.java
+++ src/test/java/com/board/Board_Upgraded/controller/AuthControllerTest_Reissue.java
@@ -1,6 +1,12 @@
 package com.board.Board_Upgraded.controller;
 
+import com.board.Board_Upgraded.repository.member.MemberRepository;
 import com.board.Board_Upgraded.service.AuthService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,5 +22,29 @@ public class AuthControllerTest_Reissue {
     @Autowired
     private AuthService authService;
 
+    @Autowired
+    private MemberRepository memberRepository;
 
+    @BeforeEach
+    void clearDB(){
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("")
+    public void reissue_Success() throws Exception{
+        //given
+
+        //expected
+
+    }
+
+    private String makeJson(Object object){
+        try{
+            return new ObjectMapper().writeValueAsString(object);
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+            return "";
+        }
+    }
 }
