94c79c8612e5165cd0a55daf4a90dd5a6bdaafb6
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-16 22:36:04 +0900

    feat: MemberController.SearchTest 검색시에 토큰을 가지고 있어도, 입력이 전부 null이면 400에러와 예외를 반환하는 것을 검증하는 테스트코드 구현

--- src/main/java/com/board/Board_Upgraded/dto/member/SearchMemberDto.java
+++ src/main/java/com/board/Board_Upgraded/dto/member/SearchMemberDto.java
@@ -2,10 +2,13 @@ package com.board.Board_Upgraded.dto.member;
 
 import com.board.Board_Upgraded.entity.member.Member;
 import com.querydsl.core.annotations.QueryProjection;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SearchMemberDto {
 
     private String username;
--- src/test/java/com/board/Board_Upgraded/controller/member/SearchTest.java
+++ src/test/java/com/board/Board_Upgraded/controller/member/SearchTest.java
@@ -1,14 +1,23 @@
 package com.board.Board_Upgraded.controller.member;
 
 import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
+import com.board.Board_Upgraded.dto.member.SearchMemberDto;
+import com.board.Board_Upgraded.dto.member.SignInRequestDto;
+import com.board.Board_Upgraded.dto.token.TokenResponseDto;
 import com.board.Board_Upgraded.repository.member.MemberRepository;
 import com.board.Board_Upgraded.service.auth.AuthService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.stream.IntStream;
 
@@ -37,6 +46,28 @@ public class SearchTest {
                                 .nickname("test" + index)
                                 .password("test" + index)
                                 .passwordCheck("test" + index).build()));
+    }
+
+    @Test
+    @DisplayName("검색 파라미터가 전부 null이면 400에러와, 검색조건이 없음을 반환한다.")
+    public void searchAllNull_FAIL() throws Exception{
+        //given
+        TokenResponseDto tokenResponseDto = authService.signIn(SignInRequestDto.builder()
+                .username("test1")
+                .password("test1").build());
+        SearchMemberDto searchMemberDto = SearchMemberDto.builder()
+                .username(null)
+                .email(null)
+                .nickname(null)
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.get("/api/members/search")
+                .header("Authorization", "Bearer ".concat(tokenResponseDto.getAccessToken()))
+                .header("RefreshToken", "Bearer ".concat(tokenResponseDto.getRefreshToken()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(searchMemberDto)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andDo(MockMvcResultHandlers.print());
     }
 
     private String makeJson(Object object){
