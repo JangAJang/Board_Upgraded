7969adbfe7ac227d21b4c73d4b1c4cd293ab479c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-18 15:39:41 +0900

    feat: MemberController.EditTest 닉네임 수정을 성공할 때, 200코드와 성공했음을 반환하는 것을 검증하는 테스트코드 구현

---
+++ src/test/java/com/board/Board_Upgraded/controller/member/EditTest.java
@@ -0,0 +1,76 @@
+package com.board.Board_Upgraded.controller.member;
+
+import com.board.Board_Upgraded.dto.member.EditMemberRequestDto;
+import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
+import com.board.Board_Upgraded.dto.member.SignInRequestDto;
+import com.board.Board_Upgraded.dto.token.TokenResponseDto;
+import com.board.Board_Upgraded.repository.member.MemberRepository;
+import com.board.Board_Upgraded.service.auth.AuthService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EditTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private AuthService authService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @BeforeEach
+    void clearDB(){
+        memberRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("닉네임 변경을 요청할 떄, 성공하면 200코드와 수정에 성공했음을 반환해준다. ")
+    public void editNickname_Success() throws Exception{
+        //given
+        authService.registerNewMember(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        TokenResponseDto tokenResponseDto = authService.signIn(SignInRequestDto.builder()
+                .username("test")
+                .password("test").build());
+        EditMemberRequestDto editMemberRequestDto = EditMemberRequestDto.builder()
+                .nickname("newNick")
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.patch("/api/members/edit")
+                .header("Authorization", "Bearer ".concat(tokenResponseDto.getAccessToken()))
+                .header("RefreshToken", "Bearer ".concat(tokenResponseDto.getRefreshToken()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(editMemberRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.data").value("수정을 성공했습니다."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    private String makeJson(Object object){
+        try{
+            return new ObjectMapper().writeValueAsString(object);
+        }catch (Exception e){
+            return "";
+        }
+    }
+}
