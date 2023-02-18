7b170b7182a3c9844c87d51d578e379b8acee84f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-18 16:50:17 +0900

    feat: MemberControllerTest.EditTest 이메일 변경을 성공하면 200코드와 성공여부를 반환하며 멤버의 인스턴스가 초기화된 것을 검증하는 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/controller/member/EditTest.java
+++ src/test/java/com/board/Board_Upgraded/controller/member/EditTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -32,6 +33,9 @@ public class EditTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @BeforeEach
     void clearDB(){
@@ -66,6 +70,36 @@ public class EditTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.result.data").value("수정을 성공했습니다."))
                 .andDo(MockMvcResultHandlers.print());
         Assertions.assertThat(memberRepository.findByUsername("test").get().getNickname()).isEqualTo("newNick");
+    }
+
+    @Test
+    @DisplayName("이메일 변경을 요청할 떄, 성공하면 200코드와 수정에 성공했음을 반환해준다. ")
+    public void editEmail_Success() throws Exception{
+        //given
+        authService.registerNewMember(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        TokenResponseDto tokenResponseDto = authService.signIn(SignInRequestDto.builder()
+                .username("test")
+                .password("test").build());
+        EditMemberRequestDto editMemberRequestDto = EditMemberRequestDto.builder()
+                .email("newMail@test.com")
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.patch("/api/members/edit")
+                .header("Authorization", "Bearer ".concat(tokenResponseDto.getAccessToken()))
+                .header("RefreshToken", "Bearer ".concat(tokenResponseDto.getRefreshToken()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(editMemberRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(true))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(200))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.data").value("수정을 성공했습니다."))
+                .andDo(MockMvcResultHandlers.print());
+        Assertions.assertThat(memberRepository.findByUsername("test").get().getEmail()).isEqualTo("newMail@test.com");
     }
 
     private String makeJson(Object object){
