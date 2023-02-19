b62092f351715bbdbb6f1bd664351dab63d30bde
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-19 23:33:48 +0900

    feat: Post 도메인 로직에서 생성자로 Post 객체를 생성하는 기능 구현

---
+++ src/main/java/com/board/Board_Upgraded/entity/post/Post.java
@@ -0,0 +1,34 @@
+package com.board.Board_Upgraded.entity.post;
+
+import com.board.Board_Upgraded.entity.base.BaseEntity;
+import com.board.Board_Upgraded.entity.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @Builder
+    public Post(String title, String content, Member member){
+        this.title = title;
+        this.content = content;
+        this.member = member;
+        setLastModifiedDate(LocalDateTime.now());
+    }
+}
