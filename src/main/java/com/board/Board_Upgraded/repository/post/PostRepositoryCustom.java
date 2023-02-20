286fae5f4fc23b512cc2aa1d5107474b3cd4076c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-20 17:51:20 +0900

    feat: PostRespositoryCustomImpl 검색 쿼리를 조건 없이 페이징처리하는 기능 구현

--- src/main/java/com/board/Board_Upgraded/repository/post/PostRepositoryCustom.java
+++ src/main/java/com/board/Board_Upgraded/repository/post/PostRepositoryCustom.java
@@ -1,9 +1,11 @@
 package com.board.Board_Upgraded.repository.post;
 
 import com.board.Board_Upgraded.dto.post.PostResponseDto;
+import com.board.Board_Upgraded.dto.post.SearchPostRequestDto;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface PostRepositoryCustom {
 
-    Page<PostResponseDto> searchPost(SearchPostRequestDto searchPostRequestDto);
+    Page<PostResponseDto> searchPost(SearchPostRequestDto searchPostRequestDto, Pageable pageable);
 }
--- src/main/java/com/board/Board_Upgraded/repository/post/PostRepositoryCustomImpl.java
+++ src/main/java/com/board/Board_Upgraded/repository/post/PostRepositoryCustomImpl.java
@@ -1,4 +1,25 @@
 package com.board.Board_Upgraded.repository.post;
 
+import com.board.Board_Upgraded.dto.post.PostResponseDto;
+import com.board.Board_Upgraded.dto.post.SearchPostRequestDto;
+import com.board.Board_Upgraded.entity.post.QPost;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+@RequiredArgsConstructor
 public class PostRepositoryCustomImpl implements PostRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Page<PostResponseDto> searchPost(SearchPostRequestDto searchPostRequestDto, Pageable pageable) {
+        queryFactory.selectFrom(QPost.post)
+                .where()
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+        return null;
+    }
 }
