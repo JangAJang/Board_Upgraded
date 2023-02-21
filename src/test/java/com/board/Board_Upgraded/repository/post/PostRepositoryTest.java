2b1f9bfc14cea96a355064235e862be7c442064f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-21 21:16:03 +0900

    feat: PostRepositoryTest 제목과 내용에서 3을 검색하면 페이지에 10개의 결과가 나오며, 중복되지 않게 총 12개를 검색하는 것을 검증하는 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/repository/post/PostRepositoryTest.java
+++ src/test/java/com/board/Board_Upgraded/repository/post/PostRepositoryTest.java
@@ -169,4 +169,36 @@ public class PostRepositoryTest {
                         "title33", "title34", "title35",
                         "title36", "title37");
     }
+
+    @Test
+    @DisplayName("제목과 내용에 3을 검색했을 때, 13, 23, 30~37이 나온다. ")
+    public void searchTest_WRITER_AND_TITLE() throws Exception{
+        //given
+        IntStream.range(1, 4).forEach(i ->
+                authService.registerNewMember(RegisterRequestDto.builder()
+                        .username("test" + i)
+                        .nickname("test" + i)
+                        .email("test" + i + "@test.com")
+                        .password("test" + i)
+                        .passwordCheck("test" + i).build()));
+        for(int index = 10; index < 40; index++){
+            Member member = memberRepository.findByUsername("test" + index/10)
+                    .orElseThrow(MemberNotFoundException::new);
+            postRepository.save(Post.builder()
+                    .member(member)
+                    .title("title" + index)
+                    .content("content" + index).build());
+        }
+        //when
+        String searchMember = "3";
+        //then
+        Assertions.assertThat(postRepository.searchPost(searchMember, TITLE_AND_CONTENT, pageRequest)
+                        .getContent().stream().map(PostResponseDto::getTitle).collect(Collectors.toList()))
+                .containsExactly(
+                        "title13",
+                        "title23", "title30", "title31", "title32",
+                        "title33", "title34", "title35",
+                        "title36", "title37");
+        Assertions.assertThat(postRepository.searchPost(searchMember, TITLE_AND_CONTENT, pageRequest).getTotalElements()).isEqualTo(12L);
+    }
 }
