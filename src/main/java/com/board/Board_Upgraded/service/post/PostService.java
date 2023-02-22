ceff6bdc8bf01bffbf12d108dbff0f1e5b7e8b86
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-22 17:00:29 +0900

    feat: PostService 게시물을 작성했을 때, 저장시키고 결과dto를 반환하는 기능 구현

--- src/main/java/com/board/Board_Upgraded/service/post/PostService.java
+++ src/main/java/com/board/Board_Upgraded/service/post/PostService.java
@@ -4,6 +4,7 @@ import com.board.Board_Upgraded.dto.post.PostResponseDto;
 import com.board.Board_Upgraded.dto.post.SearchPostRequestDto;
 import com.board.Board_Upgraded.dto.post.WritePostRequestDto;
 import com.board.Board_Upgraded.entity.member.Member;
+import com.board.Board_Upgraded.entity.post.Post;
 import com.board.Board_Upgraded.repository.member.SearchPostType;
 import com.board.Board_Upgraded.repository.post.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,7 +21,16 @@ public class PostService {
 
     @Transactional
     public PostResponseDto write(WritePostRequestDto writePostRequestDto , Member member){
-        return null;
+        Post post = Post.builder()
+                .title(writePostRequestDto.getTitle())
+                .content(writePostRequestDto.getContent())
+                .member(member).build();
+        postRepository.save(post);
+        return PostResponseDto.builder()
+                .writer(member.getNickname())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .lastModifiedDate(post.getLastModifiedDate()).build();
     }
 
     @Transactional(readOnly = true)
