55cb6fe32c64f11e91e0c8ee397393737632dc44
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-22 16:49:12 +0900

    feat: PostServiceTest 제목과 내용을 입력하면 글을 생성하는 것을 검증하는 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/service/post/PostServiceTest.java
+++ src/test/java/com/board/Board_Upgraded/service/post/PostServiceTest.java
@@ -1,8 +1,15 @@
 package com.board.Board_Upgraded.service.post;
 
+import com.board.Board_Upgraded.dto.member.RegisterRequestDto;
+import com.board.Board_Upgraded.dto.post.WritePostRequestDto;
+import com.board.Board_Upgraded.entity.member.Member;
+import com.board.Board_Upgraded.exception.member.MemberNotFoundException;
 import com.board.Board_Upgraded.repository.member.MemberRepository;
+import com.board.Board_Upgraded.repository.post.PostRepository;
 import com.board.Board_Upgraded.service.auth.AuthService;
-import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,4 +26,28 @@ public class PostServiceTest {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    @DisplayName("게시물의 제목과 내용이 있으면 글을 작성해준다. ")
+    public void writeSuccess() throws Exception{
+        //given
+        authService.registerNewMember(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test")
+                .build());
+        Member member = memberRepository.findByUsername("test").orElseThrow(MemberNotFoundException::new);
+        WritePostRequestDto writePostRequestDto = WritePostRequestDto.builder()
+                .title("취업")
+                .content("가즈아").build();
+        //when
+        postService.write(writePostRequestDto, member);
+        //then
+        Assertions.assertThat(postRepository.count()).isEqualTo(1L);
+    }
 }
