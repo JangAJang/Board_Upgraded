ee9babfb72638d6af7f458e425f52ab2c0c5af4f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-26 21:56:22 +0900

    feat: PostController 시큐리티 콘텍스트에서 아이디를 이용해 멤버 객체를 불러와 현재 사용중인 멤버가 글을 저장하게 하는 기능 구현

--- src/main/java/com/board/Board_Upgraded/controller/post/PostController.java
+++ src/main/java/com/board/Board_Upgraded/controller/post/PostController.java
@@ -1,9 +1,20 @@
 package com.board.Board_Upgraded.controller.post;
 
+import com.board.Board_Upgraded.dto.post.PostResponseDto;
+import com.board.Board_Upgraded.dto.post.WritePostRequestDto;
+import com.board.Board_Upgraded.entity.member.Member;
+import com.board.Board_Upgraded.exception.member.MemberNotFoundException;
+import com.board.Board_Upgraded.repository.member.MemberRepository;
+import com.board.Board_Upgraded.response.Response;
 import com.board.Board_Upgraded.service.post.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -11,4 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+    private final MemberRepository memberRepository;
+
+    @PostMapping("/write")
+    public Response write(@RequestBody @Valid WritePostRequestDto writePostRequestDto){
+        PostResponseDto postResponseDto = postService.write(writePostRequestDto, getCurrentMember());
+        return Response.success(postResponseDto);
+    }
+
+    private Member getCurrentMember(){
+        return memberRepository.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName())
+                .orElseThrow(MemberNotFoundException::new);
+    }
 }
