e05634f493c6b9f415daa1b36fefc732dcd0f7ed
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-27 23:19:42 +0900

    refactor: PostController, SearchPostRequestDto @Valid어노테이션으로 검색할 파라미터를 입력하지 않았을 때 예외처리가 되도록 기능 구현

--- src/main/java/com/board/Board_Upgraded/dto/post/SearchPostRequestDto.java
+++ src/main/java/com/board/Board_Upgraded/dto/post/SearchPostRequestDto.java
@@ -4,10 +4,17 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class SearchPostRequestDto {
 
+    @NotNull(message = "검색할 내용을 입력해주세요.")
+    @NotBlank(message = "검색할 내용을 입력해주세요.")
+    @NotEmpty(message = "검색할 내용을 입력해주세요.")
     private String text;
 
     public SearchPostRequestDto(String text) {
