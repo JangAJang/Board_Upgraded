44a06914c683e65f0330d727cae7db53c02f9f34
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-27 23:33:48 +0900

    refactor: WritePostRequestDto dto생성시에 입력이 null, 공백, 아무것도 없는 문자열일 때 예외처리되도록 기능 구현

--- src/main/java/com/board/Board_Upgraded/dto/post/WritePostRequestDto.java
+++ src/main/java/com/board/Board_Upgraded/dto/post/WritePostRequestDto.java
@@ -2,11 +2,21 @@ package com.board.Board_Upgraded.dto.post;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class WritePostRequestDto {
 
+    @NotNull(message = "제목을 입력하세요.")
+    @NotEmpty(message = "제목을 입력하세요.")
+    @NotBlank(message = "제목을 입력하세요.")
     private String title;
+    @NotNull(message = "내용을 입력하세요.")
+    @NotEmpty(message = "내용을 입력하세요.")
+    @NotBlank(message = "내용을 입력하세요.")
     private String content;
 
     @Builder
