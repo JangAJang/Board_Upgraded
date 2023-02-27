5d0f9e63c9087dc8d24a400de837e32c00d3dcd2
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-27 23:42:13 +0900

    feat: PostController.CreateEditDeleteTest 게시물 생성에서 내용이 null, 공백, 빈 문자열일 때에도 400에러와 예외가 발생하는 것을 검증하는 테스트코드 구현

--- src/test/java/com/board/Board_Upgraded/controller/post/CreateEditDeleteTest.java
+++ src/test/java/com/board/Board_Upgraded/controller/post/CreateEditDeleteTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -130,7 +129,7 @@ public class CreateEditDeleteTest {
     }
 
     @Test
-    @DisplayName("게시물을 작성할 때, 제목이 공백문자이면 400(bad request)에러와 제목을 입력해야함을 반환한다. ")
+    @DisplayName("게시물을 작성할 때, 제목이 빈 문자열이면 400(bad request)에러와 제목을 입력해야함을 반환한다. ")
     public void writePostTest_EmptyTitle() throws Exception{
         //given
         authService.registerNewMember(RegisterRequestDto.builder()
@@ -156,6 +155,95 @@ public class CreateEditDeleteTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(400))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.result.failMessage").value("제목을 입력하세요."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("게시물을 작성할 때, 내용이 null이면 400(bad request)에러와 제목을 입력해야함을 반환한다. ")
+    public void writePostTest_NoContent() throws Exception{
+        //given
+        authService.registerNewMember(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        TokenResponseDto tokenResponseDto = authService.signIn(SignInRequestDto.builder()
+                .username("test")
+                .password("test").build());
+        WritePostRequestDto writePostRequestDto = WritePostRequestDto.builder()
+                .title("제목입니다.")
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.post("/api/posts/write")
+                .header("Authorization", "Bearer ".concat(tokenResponseDto.getAccessToken()))
+                .header("RefreshToken", "Bearer ".concat(tokenResponseDto.getRefreshToken()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(writePostRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(400))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.failMessage").value("내용을 입력하세요."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("게시물을 작성할 때, 내용이 공백문자이면 400(bad request)에러와 제목을 입력해야함을 반환한다. ")
+    public void writePostTest_BlankContent() throws Exception{
+        //given
+        authService.registerNewMember(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        TokenResponseDto tokenResponseDto = authService.signIn(SignInRequestDto.builder()
+                .username("test")
+                .password("test").build());
+        WritePostRequestDto writePostRequestDto = WritePostRequestDto.builder()
+                .content("  ")
+                .title("제목입니다.")
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.post("/api/posts/write")
+                .header("Authorization", "Bearer ".concat(tokenResponseDto.getAccessToken()))
+                .header("RefreshToken", "Bearer ".concat(tokenResponseDto.getRefreshToken()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(writePostRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(400))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.failMessage").value("내용을 입력하세요."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @Test
+    @DisplayName("게시물을 작성할 때, 내용이 빈 문자열이면 400(bad request)에러와 제목을 입력해야함을 반환한다. ")
+    public void writePostTest_EmptyContent() throws Exception{
+        //given
+        authService.registerNewMember(RegisterRequestDto.builder()
+                .username("test")
+                .nickname("test")
+                .email("test@test.com")
+                .password("test")
+                .passwordCheck("test").build());
+        TokenResponseDto tokenResponseDto = authService.signIn(SignInRequestDto.builder()
+                .username("test")
+                .password("test").build());
+        WritePostRequestDto writePostRequestDto = WritePostRequestDto.builder()
+                .content("")
+                .title("제목입니다.")
+                .build();
+        //expected
+        mvc.perform(MockMvcRequestBuilders.post("/api/posts/write")
+                .header("Authorization", "Bearer ".concat(tokenResponseDto.getAccessToken()))
+                .header("RefreshToken", "Bearer ".concat(tokenResponseDto.getRefreshToken()))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(makeJson(writePostRequestDto)))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value(false))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(400))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result.failMessage").value("내용을 입력하세요."))
                 .andDo(MockMvcResultHandlers.print());
     }
 
