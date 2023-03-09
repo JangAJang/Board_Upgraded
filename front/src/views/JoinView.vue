fb2be7040d3a8559ba955b182bb38a67679f6f52
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-09 23:49:18 +0900

    feat: JoinView.vue 회원가입 입력을 받는 화면 생성

---
+++ front/src/views/JoinView.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const username = ref('')
+const nickname = ref('')
+const email = ref('')
+const password = ref('')
+const passwordCheck = ref('')
+
+const join = function () {
+  axios.get('http://localhost:8080')
+}
+</script>
+
+<template>
+  <div class="align-content-center">
+    <div class="mt-1">
+      <el-input v-model="username" type="text" placeholder="아이디를 입력해주세요." />
+    </div>
+    <div class="mt-1">
+      <el-input v-model="nickname" type="text" placeholder="닉네임을 입력해주세요." />
+    </div>
+    <div class="mt-1">
+      <el-input v-model="email" type="email" placeholder="이메일을 입력해주세요." />
+    </div>
+    <div class="mt-1">
+      <el-input v-model="password" type="password" placeholder="비밀번호를 입력해주세요." />
+    </div>
+    <div class="mt-1">
+      <el-input
+        v-model="passwordCheck"
+        type="password"
+        placeholder="비밀번호를 다시 입력해주세요."
+      />
+    </div>
+    <div class="mt-1">
+      <el-button type="primary" @click="join()">회원 가입</el-button>
+    </div>
+  </div>
+</template>
+
+<style></style>
