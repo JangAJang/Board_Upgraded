0035b3b5d841c813bdad0c9e3014d53d1213a8ae
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-10 18:31:00 +0900

    feat: (BackEnd)CorsConfig 프론트엔드에서 오는 CORS요청을 허가하는 기능 구현

--- src/main/java/com/board/Board_Upgraded/config/CorsConfig.java
+++ src/main/java/com/board/Board_Upgraded/config/CorsConfig.java
@@ -5,15 +5,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
 
     @Bean
     public CorsFilter corsFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5173");
         config.addAllowedOrigin("*");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
--- src/main/java/com/board/Board_Upgraded/controller/TestController.java
+++ src/main/java/com/board/Board_Upgraded/controller/TestController.java
@@ -10,20 +10,25 @@ import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/test")
 @Slf4j
 public class TestController {
 
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/")
+    public String base(){
+        return "프론트 CORS 문제 없음";
+    }
+
     @ApiOperation(value = "get메서드 테스트", notes = "get이 스웨거에 등록되는지 테스트 합니다")
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping("/getTest")
+    @GetMapping("/api/test/getTest")
     public String getController(){
         return "get";
     }
 
     @ApiOperation(value = "post메서드 테스트", notes = "post이 스웨거에 등록되는지 테스트 합니다")
     @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/postTest")
+    @PostMapping("/api/test/postTest")
     public String postController(@RequestBody String testUser){
         log.info("testUser = {}, testTime = {}", testUser, LocalDateTime.now());
         return "post";
@@ -31,11 +36,11 @@ public class TestController {
 
     @ApiOperation(value = "put메서드 테스트", notes = "put이 스웨거에 등록되는지 테스트 합니다")
     @ResponseStatus(HttpStatus.OK)
-    @PutMapping("/putTest")
+    @PutMapping("/api/test/putTest")
     public String putController(){return "put";}
 
     @ApiOperation(value = "delete메서드 테스트", notes = "delete이 스웨거에 등록되는지 테스트 합니다")
     @ResponseStatus(HttpStatus.OK)
-    @DeleteMapping("/deleteTest")
+    @DeleteMapping("/api/test/deleteTest")
     public String deleteController(){return "delete";}
 }
