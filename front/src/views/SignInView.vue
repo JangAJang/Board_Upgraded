a80be86fba350869ee7954affd0f4d73a91a3856
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-10 18:55:53 +0900

    feat: (FrontEnd)SignInView 로그인을 수행하는 프론트 화면 생성 및 로그인 성공 확인

---
+++ front/src/views/SignInView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+
+const username = ref('')
+const password = ref('')
+
+const signIn = function () {
+  axios.post('http://localhost:8080/api/auth/sign_in', {
+    username:username.value,
+    password:password.value
+  })
+}
+</script>
+
+<template>
+  <div class="align-content-center">
+    <div class="mt-1">
+      <el-input v-model="username" type="text" placeholder="아이디를 입력해주세요." />
+    </div>
+    <div class="mt-1">
+      <el-input v-model="password" type="password" placeholder="비밀번호를 입력해주세요." />
+    </div>
+    <div class="mt-1">
+      <el-button type="primary" @click="signIn()">로그인</el-button>
+    </div>
+  </div>
+</template>
+
+<style></style>
