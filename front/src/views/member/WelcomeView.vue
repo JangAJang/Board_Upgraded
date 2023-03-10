6cdd024557a247c926635723815424988106070c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-03-10 23:02:41 +0900

    feat: (FrontEnd) WelcomeView.vue 로그인 성공시에, 환영 페이지 생성

---
+++ front/src/views/member/WelcomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import axios from 'axios';
+
+const welcome = {
+  message: "회원1번가"
+}
+
+axios.get("/members/welcome").then((response)=> {
+  console.log(response)
+});
+
+</script>
+
+<template>
+  <div class="align-content-center">
+    {{ welcome.message }}
+  </div>
+</template>
+
+<style>
+</style>
